8d211f2e6ddb4e64311c4b29e3d3c84058a8e877
NidhalChelhi <nidhalelchelhi@gmail.com>
2024-01-15 19:54:37 +0100

    Added Form Validation Toasts

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,10 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
 const montserrat = Montserrat({ subsets: ["latin"] });
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata = {
   title: "Melkart JE",
@@ -17,6 +19,17 @@ export default function RootLayout({ children }) {
         className={`${montserrat.className} bg-primary overflow-x-hidden relative z-0  `}
       >
         <Navbar />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
         {children}
         <Footer />
       </body>
