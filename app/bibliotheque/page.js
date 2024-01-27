b399d0ffe0848eac5418ac3a1e907cb33d1ec2d2
wajdmehdaoui2001 <wajdmahdaoui@gmail.com>
2024-01-27 19:31:33 +0100

    page bibliothèque changed

--- app/bibliotheque/page.js
+++ app/bibliotheque/page.js
@@ -1,27 +1,23 @@
 import Head from 'next/head';
 
 export default function Bibliothèque() {
-  // Supposons que vous avez un tableau de données pour chaque cadre
+  
   const data = [
     {
-      title: "Dossier de Partenariat Mandat 2023-2024",
+      title: "Etats Financiers Melkart 2023-2024",
       description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum euismod bibendum. Curabitur id justo ut libero tristique tincidunt. Fusce consectetur augue ac dolor dignissim, vel lacinia ligula tincidunt. Sed ac mauris sit amet nunc malesuada fermentum. In hac habitasse platea dictumst. Integer in sapien vel justo pharetra ultrices. Quisque vitae vestibulum libero. Nullam euismod orci id cursus pharetra. Etiam id libero ac elit cursus tincidunt.",
-      image: "/images/document.png",
+        "",
+      image: "/images/etatfinanciers.jpg",
+      lien:"/pdfs/ETATS-FINANCIERS-MELKART-22-23.pdf"
     },
     {
-      title: "Dossier de Partenariat Mandat 2023-2024",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum euismod bibendum. Curabitur id justo ut libero tristique tincidunt. Fusce consectetur augue ac dolor dignissim, vel lacinia ligula tincidunt. Sed ac mauris sit amet nunc malesuada fermentum. In hac habitasse platea dictumst. Integer in sapien vel justo pharetra ultrices. Quisque vitae vestibulum libero. Nullam euismod orci id cursus pharetra. Etiam id libero ac elit cursus tincidunt.",
-      image: "/images/document.png",
+      title: "Politique Qualité",
+      description: "La MELKART Junior Entreprise est une association étudiante à but non lucratif, établie au sein de l'École Nationale d'Ingénieurs de Carthage, qui a pour objectif principal de promouvoir l'insertion professionnelle des jeunes étudiants et de renforcer leurs compétences à travers la réalisation de projets concrets. Depuis Mai 2021, la MELKART Junior Entreprise s’engage à l’implémentation d’un système de management de la qualité pour améliorer la performance de l’entité. Dans cette optique, nous avons mis en place un système de management, basé sur l’approche risques pour détecter toute défaillance et agir en question dans le but d’assurer l’amélioration continue de l'entité.",
+      image: "/images/politiqueQualite.jpg",
+      lien:"/pdfs/MJE23_POL_Qu_01.pdf"
     },
-    {
-      title: "Dossier de Partenariat Mandat 2023-2024",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum euismod bibendum. Curabitur id justo ut libero tristique tincidunt. Fusce consectetur augue ac dolor dignissim, vel lacinia ligula tincidunt. Sed ac mauris sit amet nunc malesuada fermentum. In hac habitasse platea dictumst. Integer in sapien vel justo pharetra ultrices. Quisque vitae vestibulum libero. Nullam euismod orci id cursus pharetra. Etiam id libero ac elit cursus tincidunt.",
-      image: "/images/document.png",
-    },
-    // Ajoutez d'autres objets de données ici
+    
+    
   ];
 
   return (
@@ -30,14 +26,13 @@ export default function Bibliothèque() {
         <div className="flex flex-col gap-4 items-center justify-center text-white py-20 text-center ">
           <h1 className="font-bold text-5xl">Bibliothèque</h1>
           <p className="font-light text-xl">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vestibulum euismod bibendum. Curabitur id justo ut
-            libero tristique tincidunt !
+            
           </p>
         </div>
         <div className="bg-white min-h-screen py-20 px-4 sm:px-8 lg:px-12 flex flex-col items-center justify-start gap-12">
           {data.map((item, index) => (
+            <a href={item.lien} key={index} target="_blank" rel="noopener noreferrer">
             <div
-              key={index}
               className="rounded-lg border-2 border-primary flex flex-col sm:flex-row container mx-auto p-4 sm:p-8 gap-8 items-start hover:scale-[1.01] app_transition"
             >
               <div className="bg-white flex items-center gap-8">
@@ -50,6 +45,7 @@ export default function Bibliothèque() {
                 </div>
               </div>
             </div>
+            </a>
           ))}
         </div>
       </div>
