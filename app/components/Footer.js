9ee09556dbb7dfe5ceaf06c6cc04cb863bbf02c7
Salma <bensaadsalma634@gmail.com>
2024-05-26 22:04:49 +0100

    commit navbar footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,69 +1,92 @@
 import React from "react";
-import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="min-h-[40vh] bg-secondary flex flex-col items-center justify-center gap-10 overflow-x-hidden">
-      <div className="flex flex-col md:flex-row items-center justify-center  ">
-        <div className="max-w-[408px] w-full">
-          <img src="/logo/logo_white.png" className="object-contain" />
+    <footer className="min-h-[40vh] flex flex-col items-center justify-center gap-5 overflow-x-hidden" style={{ backgroundColor: "#211902" }}>
+      <div className="flex flex-col md:flex-row items-center justify-center gap-6 w-full px-4">
+      {/* Partie gauche avec le logo */}
+      <div className="max-w-[408px] w-full md:w-1/4 ml-3 mt-5">
+          <img src="/logo/Logo 1.png" className="object-contain h-34" />
         </div>
-        <div className="flex flex-col px-4 md:px-0 items-center md:items-start justify-center gap-8">
-          <Link target="_blank" href="tel:+21629619677">
-            <div className="flex items-center  gap-2 md:gap-4">
-              <img src="/icons/phone.svg" className="h-6 md:h-10" />
-              <span className="text-white lg:text-2xl ">
-                +216 29 619 677 | +216 92 631 204
-              </span>
-            </div>
-          </Link>
-          <Link
-            target="_blank"
-            href="mailto:enicarthage.junior.entreprise@gmail.com"
-          >
-            <div className="flex items-center  gap-2 md:gap-4">
-              <img src="/icons/mail.svg" className="h-6 md:h-10" />
-              <span className="text-white lg:text-2xl ">
-                enicarthage.junior.entreprise@gmail.com
-              </span>
-            </div>
-          </Link>
-          <Link
-            target="_blank"
-            href="https://www.google.com/maps/place/National+Engineering+School+of+Carthage/@36.851767,10.2085734,17z/data=!3m1!4b1!4m6!3m5!1s0x12fd34d69e694547:0xe1e0376f3637688f!8m2!3d36.851767!4d10.2111483!16s%2Fg%2F1218dkmv?entry=ttu"
-          >
-            <div className="flex items-center  gap-2 md:gap-4">
-              <img src="/icons/map.svg" className="h-6 md:h-10" />
-              <span className="text-white lg:text-2xl ">
-                45 Rue des Entrepreneurs 2035 Charguia II , 2035
-              </span>
-            </div>
-          </Link>
-          <div className="flex items-center  justify-center gap-4">
-            <Link
-              target="_blank"
-              href="https://www.linkedin.com/company/melkart-je/?originalSubdomain=tn"
-            >
-              <img src="/icons/linkedin.svg" className="h-8 md:h-10" />
-            </Link>
 
-            <Link
+        {/* Partie centrale avec les réseaux sociaux */}
+        <div className="flex flex-col items-center md:items-start justify-center gap-4 w-full md:w-1/4 md:ml-2">
+          <p className="text-white text-left mb-2 text-sm">
+            Cabinet de conseil spécialisé dans le domaine de l'agronomie. 
+            <p>Depuis 2012.</p>
+          </p>
+          <div className="flex items-center justify-start gap-4">
+            <a
               target="_blank"
-              href="https://www.instagram.com/melkart_je/?hl=en"
+              href="https://www.linkedin.com/company/inat-junior-entreprise/"
             >
-              <img src="/icons/instagram.svg" className="h-8 md:h-10" />
-            </Link>
+              <img src="/icons/linkedin.svg" className="h-5 md:h-7" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.instagram.com/inat_junior_entreprise/"
+            >
+              <img src="/icons/instagram.svg" className="h-5 md:h-7" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.facebook.com/INAT.Junior.Entreprise"
+            >
+              <img src="/icons/facebook.svg" className="h-5 md:h-7" />
+            </a>
+          </div>
+        </div>
 
-            <Link
-              target="_blank"
-              href="https://www.facebook.com/Enicarthage.Junior.Entreprise/?locale=fr_FR"
-            >
-              <img src="/icons/facebook.svg" className="h-8 md:h-10" />
-            </Link>
+
+       {/* Partie "Explore" avec les liens */}
+       <div className="flex flex-col items-start justify-center gap-2 w-full md:w-1/4 md:ml-14 md:mr-2 md:mt-6">
+          <h3 className="text-white font-bold mb-1 rounded-full">Explore</h3>
+          <div className="w-10 h-1 bg-yellow-500 mb-2"></div>
+          <div className="flex items-center gap-2">
+            <img src="/logo/feuille.png" className="h-4 w-4" alt="Icon" />
+            <a href="/prestations" className="text-white hover:text-gray-300 text-sm">Prestations</a>
+          </div>
+          <div className="flex items-center gap-2">
+            <img src="/logo/feuille.png" className="h-4 w-4" alt="Icon" />
+            <a href="/evenements" className="text-white hover:text-gray-300 text-sm">Événements</a>
+          </div>
+          <div className="flex items-center gap-2">
+            <img src="/logo/feuille.png" className="h-4 w-4" alt="Icon" />
+            <a href="/articles" className="text-white hover:text-gray-300 text-sm">Articles</a>
+          </div>
+          <div className="flex items-center gap-2">
+            <img src="/logo/feuille.png" className="h-4 w-4" alt="Icon" />
+            <a href="/bibliotheque" className="text-white hover:text-gray-300 text-sm">Bibliothèque</a>
+          </div>
+        </div>
+
+       {/* Partie droite avec les informations de contact */}
+       <div className="flex flex-col items-start justify-center gap-2 w-full md:w-1/4 md:ml-5 md:mr-24 md:mt-4">
+          <h3 className="text-white font-bold mb-1">Contact</h3>
+          <div className="w-10 h-1 bg-yellow-500 mb-2 rounded-full"></div>
+          <div className="flex items-center gap-2 mb-1">
+            <img src="/logo/tel.png" className="h-4 w-4" alt="Phone Icon" />
+            <span className="text-white text-sm">
+              +216 53 471 630 | +216 55 414 262
+            </span>
+          </div>
+          <div className="flex items-center gap-2 mb-1">
+            <img src="/logo/mail.png" className="h-4 w-4" alt="Email Icon" />
+            <span className="text-white text-sm">
+              contact1.inatje@gmail.com
+            </span>
+          </div>
+          <div className="flex items-center gap-2 mb-1">
+            <img src="/logo/loc.png" className="h-5 w-5" alt="Location Icon" />
+            <span className="text-white text-sm">
+              Cité Mahrajen, Manzah1 INAT Tunis
+            </span>
           </div>
         </div>
       </div>
-      <span className="text-white text-center pb-4">
-        &copy; Copyright 2023 Melkart Junior Entreprise All Rights Reserved.
+
+      <span className="text-white text-center pb-4 text-sm">
+        &copy; All Copyright 2024 INAT JE
       </span>
     </footer>
   );
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -5,35 +5,101 @@ import Link from "next/link";
 
 export default function Navbar() {
   const [toggle, setToggle] = useState(false);
+  const [showPrestationsList, setShowPrestationsList] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowPrestationsList(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowPrestationsList(false);
+  };
+
   const navigationLinks = [
     { title: "Accueil", route: "/" },
-    { title: "Notre équipe", route: "/team" },
-    { title: "Demander un devis", route: "/devis" },
+    {
+      title: "Nos Prestations",
+      route: "#",
+      onMouseEnter: handleMouseEnter,
+      onMouseLeave: handleMouseLeave,
+      prestations: ["Prestation 1", "Prestation 2", "Prestation 3"],
+    },
+    { title: "Nos Événements", route: "/events" },
+    { title: "Articles", route: "/inat-academy" },
     { title: "Bibliothèque", route: "/bibliotheque" },
+    { title: "Demander un devis", route: "/devis" },
     { title: "Contact", route: "/contact" },
   ];
 
   return (
-    <nav className="w-full h-20 flex justify-between items-center fixed z-[50] px-8 py-4 bg-primary/90 bg-opacity-0 backdrop-blur-lg shadow-lg">
+    <nav className="w-full h-20 flex justify-between items-center fixed z-[50] px-8 py-4 bg-transparent shadow-lg text-sm">
       <div className="flex h-20 justify-start items-center ">
         <Link href="/">
           <img
-            src="/logo/logo_white.png"
+            src="/logo/logo 1.png"
             alt="MJE Logo"
             className="h-auto w-24 "
           />
         </Link>
       </div>
-      <ul className="hidden lg:flex flex-1 justify-center items-center list-none">
+
+      <ul className="flex lg:hidden justify-center items-center list-none relative">
         {navigationLinks.map((item) => (
           <li
-            className=" cursor-pointer mx-5 app_transition text-white flex-col text-2xl app_transition hover:text-secondary font-medium select-none"
+            className="cursor-pointer mx-5 app_transition text-white flex-col text-lg app_transition hover:text-secondary font-medium select-none relative"
             key={`link-${item.title}`}
+            onMouseEnter={item.onMouseEnter}
+            onMouseLeave={item.onMouseLeave}
           >
             <Link href={`${item.route}`}>{item.title}</Link>
+            {item.title === "Nos Prestations" && showPrestationsList && (
+              <ul className="absolute top-0 left-0 bg-yellow-300 bg-opacity-20 w-40 mt-2 list-none p-2 rounded-lg">
+                {item.prestations.map((prestation, index) => (
+                  <li
+                    key={index}
+                    className="cursor-pointer text-white hover:text-black"
+                  >
+                    {prestation}
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
+
+      <div className="hidden lg:flex items-center relative">
+        <ul className="flex justify-center items-center list-none">
+          {navigationLinks.map((item) => (
+            <li
+              className="cursor-pointer mx-5 app_transition text-white flex-col text-lg app_transition hover:text-secondary font-medium select-none relative"
+              key={`link-${item.title}`}
+              onMouseEnter={item.onMouseEnter}
+              onMouseLeave={item.onMouseLeave}
+            >
+              <Link href={`${item.route}`}>{item.title}</Link>
+              {item.title === "Nos Prestations" && showPrestationsList && (
+                <ul className="absolute top-0 left-0 bg-yellow-300 bg-opacity-20 w-40 mt-7 list-none p-2 rounded-lg">
+                  {item.prestations.map((prestation, index) => (
+                    <li
+                      key={index}
+                      className="cursor-pointer text-white hover:text-black"
+                    >
+                      {prestation}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+
+        <img
+          src="/logo/Slogan.png"
+          alt="Logo"
+          className="h-auto w-24 ml-10"
+        />
+      </div>
 
       <div className="lg:hidden w-[35px] h-[35px] relative flex justify-center items-center ">
         {!toggle && (
@@ -58,7 +124,10 @@ export default function Navbar() {
                   <a
                     className="text-white no-underline text-lg  app_transition hover:text-secondary select-none"
                     href={`${item.route}`}
-                    onClick={() => setToggle(false)}
+                    onClick={() => {
+                      setToggle(false);
+                      item.onClick && item.onClick();
+                    }}
                   >
                     {item.title}
                   </a>
